76bc878c356be3c2f740328346efd012c5d81acf
Ilya Ezhov <bah6ntowl@gmail.com>
2023-11-18 22:02:13 +0300

    change calculating

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,18 +2,28 @@ import java.awt.geom.Arc2D;
 
 public class Calculate {
     public static double getAbbreviatedResult(int startYear) {
-        int counter = 1;
-        double sum = 0.0;
-        for (int year = startYear; year < 2022; year++) {
-            double startIndex = Constants.MOEX_RATE[year - 2002];
-            double indexing = Math.pow((6731.43 / startIndex), 1/counter);
-            double profit = (indexing - 1) * 100;
-            double inflation = Constants.INFLATION_RATE[year - 2002];
-            double percentageOfProfit = profit - inflation;
-            sum += percentageOfProfit;
-            counter++;
+        double result = 0.0;
+
+        for (double percentageOfSpending = 0.04; percentageOfSpending <= 100; percentageOfSpending += 0.04) {
+            double budget = 1.0;
+            double capital = 100 / percentageOfSpending;
+
+
+            for (int year = startYear; year < 2022; year++) {
+                capital -= budget;
+                double startIndex = Constants.MOEX_RATE[year - 2002];
+                double finalIndex = Constants.MOEX_RATE[year + 1 - 2002];
+                double profit = (finalIndex - startIndex) / startIndex * 100;
+                capital += capital * profit / 100;
+                budget += budget * Constants.INFLATION_RATE[year - 2002] / 100;
+            }
+
+            if (capital < 0) {
+                break;
+            } else {
+                result = percentageOfSpending;
+            }
         }
-        double result = sum / counter;
         double abbreviatedResult = Math.round(result * 2) / 2.0;
         return abbreviatedResult;
     }
