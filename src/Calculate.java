4202e9e1f2d6cef931f55cca307522056678f848
Ilya Ezhov <bah6ntowl@gmail.com>
2023-11-24 18:04:00 +0300

    split calculate class on methods

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,30 +1,38 @@
-import java.awt.geom.Arc2D;
-
 public class Calculate {
-    public static double getAbbreviatedResult(int startYear) {
+    private static int startYear = 2002;
+    private static int finalYear = 2022;
+    public static double getAbbreviatedResult(int inputYear) {
         double result = 0.0;
 
-        for (double percentageOfSpending = 0.04; percentageOfSpending <= 100; percentageOfSpending += 0.04) {
-            double budget = 1.0;
-            double capital = 100 / percentageOfSpending;
-
-
-            for (int year = startYear; year < 2022; year++) {
-                capital -= budget;
-                double startIndex = Constants.MOEX_RATE[year - 2002];
-                double finalIndex = Constants.MOEX_RATE[year + 1 - 2002];
-                double profit = (finalIndex - startIndex) / startIndex * 100;
-                capital += capital * profit / 100;
-                budget += budget * Constants.INFLATION_RATE[year - 2002] / 100;
-            }
-
+        for (double percentageOfSpending = 0.5; percentageOfSpending <= 100; percentageOfSpending += 0.5) {
+            double capital = calculateCapital(percentageOfSpending, inputYear);
             if (capital < 0) {
                 break;
             } else {
                 result = percentageOfSpending;
             }
         }
-        double abbreviatedResult = Math.round(result * 2) / 2.0;
-        return abbreviatedResult;
+
+        return result;
+    }
+    private static double calculateCapital(double percentageOfSpending, int inputYear) {
+        double budget = 1.0;
+        double capital = 100 / percentageOfSpending * budget;
+
+        for (int year = inputYear; year < finalYear; year++) {
+            capital -= budget;
+            double profit = calculateProfit(year);
+            capital += capital * profit / 100;
+            budget += budget * Constants.INFLATION_RATE[year - startYear] / 100;
+        }
+
+        return capital;
+    }
+
+    private static double calculateProfit(int year) {
+        double startIndex = Constants.MOEX_RATE[year - 2002];
+        double finalIndex = Constants.MOEX_RATE[year + 1 - 2002];
+        double profit = (finalIndex - startIndex) / startIndex * 100;
+        return profit;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    private static int startYear = 2002;
+    private static int finalYear = 2022;
     public static void main(String[] args) throws IllegalArgumentException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год начала жизни на проценты с 2002 по 2021 включительно");
         String input = scanner.nextLine();
-        int startYear = Integer.parseInt(input);
-        if(startYear < 2002 || startYear >= 2022){
+        int inputYear = Integer.parseInt(input);
+        if(inputYear < startYear || inputYear >= finalYear){
             throw new IllegalArgumentException();
         }
-        double abbreviatedResult = Calculate.getAbbreviatedResult(startYear);
+        double abbreviatedResult = Calculate.getAbbreviatedResult(inputYear);
         System.out.println(abbreviatedResult);
     }
 }
